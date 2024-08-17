8fa5bd718ab9ab4d7a36e65afe0a3485f672e8de
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2024-08-17 22:59:55 +0200

    rubocop: silence classLength in test

--- test/test_i_do_not_need_interactor.rb
+++ test/test_i_do_not_need_interactor.rb
@@ -2,7 +2,7 @@
 
 require "test_helper"
 
-class TestIDoNotNeedInteractor < Minitest::Test
+class TestIDoNotNeedInteractor < Minitest::Test # rubocop:disable Metrics/ClassLength
   def test_that_it_has_a_version_number
     refute_nil ::IDoNotNeedInteractor::VERSION
   end
